5d2a4e66860febbde1f6d2c77aba2a671df1852f
Vivian <info.vivianguo@gmail.com>
2023-11-02 00:37:12 -0400

    added bootstrap and completed v1 of header component;

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,21 @@
 <template>
-  <!-- Side menu -->
-  <div></div>
-  <!-- Logo -->
-  <div><img src="~/assets/images/logo/wbr-logo.svg" class="logo" /></div>
-  <!-- Navigation -->
-  <div></div>
+  <div class="container header">
+    <div class="row">
+      <!-- Side menu -->
+      <div class="col-1">
+        <img src="~/assets/images/icons/menu.svg" class="icon" />
+      </div>
+      <!-- Logo -->
+      <div class="col-2">
+        <img src="~/assets/images/logo/wbr-logo.svg" class="logo" />
+      </div>
+      <!-- Navigation -->
+      <div class="col-9 navigation">
+        <a href="#">ALUMNI INSIGHTS</a>
+        <a href="#">BUSINESS STRATEGY</a>
+        <a href="#">ENTREPENEURSHIP</a>
+        <a href="#">TECHNOLOGY</a>
+      </div>
+    </div>
+  </div>
 </template>
