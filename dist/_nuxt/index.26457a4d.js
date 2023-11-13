5a5d830cb699bdfcba24a8db913973db65065720
Vivian <info.vivianguo@gmail.com>
2023-11-13 00:08:50 -0500

    dist

---
+++ dist/_nuxt/index.26457a4d.js
@@ -0,0 +1,1 @@
+import{u as n}from"./vue.f36acd1f.91dd9d6d.js";import{u as s}from"./entry.9dc8cdf3.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return n({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const u=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:u}}})}export{i as u};
--- dist/_nuxt/index.8403faf2.js
+++
@@ -1,1 +0,0 @@
-import{u as n}from"./vue.f36acd1f.c680700a.js";import{u as s}from"./entry.90564790.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return n({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const u=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:u}}})}export{i as u};
